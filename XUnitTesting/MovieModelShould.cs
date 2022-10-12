0678c5bb887e9146284b5acde5c1f08e78362d1e
miky14n <miky14n@gmail.com>
2022-10-12 01:04:10 -0400

    Creando pruebas unitarias para MovieModel

--- XUnitTesting/MovieModelShould.cs
+++ XUnitTesting/MovieModelShould.cs
@@ -20,5 +20,67 @@ namespace XUnitTesting
             //Assert
             Assert.Equal(movieId, getValue);
         }
+
+        [Fact]
+        public void ValidateMovieSetTitle()
+        {
+            //Arrange
+            MovieModel movie = new MovieModel();
+            string title = "Avengers1";
+            movie.Title =title;
+            //Act
+            var getValue=movie.Title;
+            //Assert
+           // Assert.Equal(movie.Title, title);
+            Assert.Contains(getValue, movie.Title);
+        }
+
+        [Fact]
+        public  void ValidateDuration()
+        {
+            //Arrange
+            MovieModel movie = new MovieModel();
+            DateTime duration = new DateTime(0, 0, 1, 20, 32, 0);
+            movie.Duration = duration;
+            //Act
+            var getValue = movie.Duration;
+            Console.WriteLine(getValue);
+            //Assert
+            Assert.IsType<DateTime?>(getValue);
+        }
+        [Fact]
+        public void ValidateGross()
+        {
+            MovieModel movie = new MovieModel();
+            movie.Gross = 1515.15f;
+
+            Assert.IsType<float>(movie.Gross);
+        }
+        [Fact]
+        public void ValidateDescription()
+        {
+            MovieModel movie = new MovieModel();
+            movie.Description = "Los vengadores son los heroes mas poderosos del planeta";
+            var getValue=movie.Description;
+            Assert.IsType<String>(getValue);
+            Assert.Contains( "heroes", getValue);
+        }
+        [Fact]
+        public void ValidateImagePath()
+        {
+
+        }
+        [Fact]
+        public void ValidateFilmFranchiseId()
+        {
+            MovieModel movie=new MovieModel();
+            FilmFranchiseModel filmFranchise = new FilmFranchiseModel();
+            filmFranchise.Id = 1;
+            movie.FilmFranchiseId = filmFranchise.Id;
+
+            Assert.Equal(filmFranchise.Id, movie.FilmFranchiseId);
+
+        }
+
     }
 }
--- XUnitTesting/MovieServiceShould.cs
+++ XUnitTesting/MovieServiceShould.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using FilmFranchiseAPI.Data;
+using FilmFranchiseAPI.Data.Entities;
+using FilmFranchiseAPI.Data.Repository;
+using FilmFranchiseAPI.Services;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -6,5 +12,26 @@ namespace XUnitTesting
 {
     public class MovieServiceShould
     {
+        [Fact]
+        public void TestValidateFranchiseAsync()
+        {
+            /*var movieEntity = new MovieEntity()
+            {
+                Id = 1,
+                Title = "MARVEL ENT",
+                Duration = new DateTime(2001, 3, 2),
+                Gross = 99.9f,
+                Description = "MARVEL",
+
+            };
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperProfile>());
+            var mapper = config.CreateMapper();
+            var movieRepositoryMock = new Mock<FilmFranchiseRepository>();
+            movieRepositoryMock.Setup(r => r.GetMovieAsync(1, 1)).ReturnsAsync(movieEntity);
+
+            var MovieService = new MovieService(movieRepositoryMock.Object, mapper);
+            var movie = await MovieService.GetMoviesAsync();
+            Assert.Equal(1, movie.Id);*/
+        }
     }
 }
