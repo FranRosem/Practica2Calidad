b8ca62f21e75714f16b6ef37ae9296e48a703159
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-14 17:54:07 -0400

    Error Exception Put Franchise Form in ControllersUT

--- XUnitTesting/ControllersUT/FilmFranchiseControllerShould.cs
+++ XUnitTesting/ControllersUT/FilmFranchiseControllerShould.cs
@@ -654,5 +654,57 @@ namespace XUnitTesting.ControllersUT
             Assert.NotNull(badResult);
             Assert.Equal(404, badResult.StatusCode);
         }
+
+        [Fact]
+        public async Task ErrorPutFranchiseAsync()
+        {
+            var positionOfBin = Directory.GetCurrentDirectory().IndexOf("bin");
+            var positionOfFolderProyect = Directory.GetCurrentDirectory().IndexOf("XUnitTesting");
+
+            var baseFileRoute = Directory.GetCurrentDirectory().Substring(0, positionOfBin - 1);
+            var baseProyectRoute = Directory.GetCurrentDirectory().Substring(0, positionOfFolderProyect - 1);
+
+            var pathOfImage = Path.Combine(baseFileRoute, "Images", "test_image.png");
+            var pathOfAPI = Path.Combine(baseProyectRoute, "FilmFranchiseAPI");
+
+            var stream = File.OpenRead(pathOfImage);
+            var file = new FormFile(stream, 0, stream.Length, null, Path.GetFileName(stream.Name))
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "application/png"
+            };
+            var actualDirectory = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(pathOfAPI);
+            Directory.SetCurrentDirectory(actualDirectory);
+
+            var fileService = new FileService();
+            var imagePath = fileService.UploadFile(file);
+
+            var filmFranchiseFormModel = new FilmFranchiseFormModel()
+            {
+                Id = 1,
+                Franchise = "Marvel",
+                FilmProductor = "Disney",
+                FilmProducer = "Kevin Feige",
+                FirstMovieYear = 2010,
+                LastMovieYear = 2022,
+                Description = "SuperHeros Movies",
+                MovieCount = 22,
+                Image = file,
+                ImagePath = imagePath
+            };
+
+            var franchiseServiceMock = new Mock<IFilmFranchiseService>();
+            var exception = new Exception("Something happend: Something happend: ..");
+            franchiseServiceMock.Setup(f => f.UpdateFranchiseAsync(1, filmFranchiseFormModel)).Throws(exception);
+
+            var franchiseController = new FilmFranchisesController(franchiseServiceMock.Object, fileService);
+
+            var result = await franchiseController.PutFranchiseAsync(1, filmFranchiseFormModel);
+            var badResult = result.Result as ObjectResult;
+
+            Assert.NotNull(badResult);
+            Assert.Equal(500, badResult.StatusCode);
+        }
     }
 }
