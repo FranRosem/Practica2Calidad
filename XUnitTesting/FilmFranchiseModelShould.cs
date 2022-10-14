545a54e35f5fc20fbe2f6afc308cc09633429f90
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-10-13 20:01:44 -0400

    Adding Unit testing for id field

---
+++ XUnitTesting/FilmFranchiseModelShould.cs
@@ -0,0 +1,23 @@
+﻿using FilmFranchiseAPI.Models;
+using Xunit;
+
+namespace XUnitTesting
+{
+    public class FilmFranchiseModelShould
+    {
+        [Fact]
+        public void ValidateFilmFranchiseSetAndGetId()
+        {
+            //Arrange
+            var ValidFilmFranchise = new FilmFranchiseModel();
+            int ValidFilmFranchiseId = 1;
+
+            //Act
+            ValidFilmFranchise.Id = ValidFilmFranchiseId;
+            bool idIsCorrect = ValidFilmFranchise.Id == ValidFilmFranchiseId;
+
+            //Assert
+            Assert.True(idIsCorrect, $"The id value: {ValidFilmFranchiseId} is not the same as {ValidFilmFranchise.Id}. It was not set correctly");
+        }
+    }
+}
