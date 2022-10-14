c28c7c8a5e3dada11237c4697d937f3eede5fb71
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-10-14 15:15:43 -0400

    Adding UT for LoginViewModel

---
+++ XUnitTesting/ModelsUT/Security/LoginViewModelShould.cs
@@ -0,0 +1,24 @@
+﻿using FilmFranchiseAPI.Models.Security;
+using Xunit;
+
+namespace XUnitTesting.ModelsUT.Security
+{
+    public class LoginViewModelShould
+    {
+        [Fact]
+        public void ValidateLoginViewModelSetAndGetEmail()
+        {
+            var loginModel = new LoginViewModel();
+            loginModel.Email = "testEmail@ucb.edu.bo";
+            Assert.True(loginModel.Email == "testEmail@ucb.edu.bo", "The Email field was not saved.");
+        }
+
+        [Fact]
+        public void ValidateLoginViewModelSetAndGetPassword()
+        {
+            var loginModel = new LoginViewModel();
+            loginModel.Password = "test_password123";
+            Assert.True(loginModel.Password == "test_password123", "The Password field was not saved.");
+        }
+    }
+}
