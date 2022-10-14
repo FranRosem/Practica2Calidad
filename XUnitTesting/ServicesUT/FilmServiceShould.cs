093f29976fb03a1d4ef46939078a491f35ec70ac
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-10-13 23:39:29 -0400

    Adding UT for UploadFile when its lenght is equal to 0

--- XUnitTesting/ServicesUT/FilmServiceShould.cs
+++ XUnitTesting/ServicesUT/FilmServiceShould.cs
@@ -32,5 +32,30 @@ namespace XUnitTesting.ServicesUT
 
             Assert.Null(exception);
         }
+        [Fact]
+        public void ValidateUploadFileWhenFileLenghtIsLessThan0()
+        {
+            var positionOfBin = Directory.GetCurrentDirectory().IndexOf("bin");
+            var positionOfFolderProyect = Directory.GetCurrentDirectory().IndexOf("XUnitTesting");
+
+            var baseFileRoute = Directory.GetCurrentDirectory().Substring(0, positionOfBin - 1);
+            var baseProyectRoute = Directory.GetCurrentDirectory().Substring(0, positionOfFolderProyect - 1);
+
+            var pathOfImage = Path.Combine(baseFileRoute, "Images", "test_image.png");
+            var pathOfAPI = Path.Combine(baseProyectRoute, "FilmFranchiseAPI");
+
+            var stream = File.OpenRead(pathOfImage);
+            var file = new FormFile(stream, 0, 0, null, Path.GetFileName(stream.Name))
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "application/png"
+            };
+            var fileService = new FileService();
+            Directory.SetCurrentDirectory(pathOfAPI);
+
+            var imagePath = fileService.UploadFile(file);
+
+            Assert.True(imagePath == string.Empty, "File was saved with lenght 0, which is not allowed!");
+        }
     }
 }
