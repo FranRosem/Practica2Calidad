bcaf3a5ff900128f693419272ea1d9e8e4f47fef
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-14 02:00:27 -0400

    Get Franchises in ControllersUT

---
+++ XUnitTesting/ControllersUT/FilmFranchiseControllerShould.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using FilmFranchiseAPI.Controllers;
+using FilmFranchiseAPI.Data;
+using FilmFranchiseAPI.Data.Entities;
+using FilmFranchiseAPI.Exceptions;
+using FilmFranchiseAPI.Models;
+using FilmFranchiseAPI.Models.Security;
+using FilmFranchiseAPI.Services;
+using FilmFranchiseAPI.Services.Security;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTesting.ControllersUT
+{
+    public class FilmFranchiseControllerShould
+    {
+        [Fact]
+        public async Task GetFranchisesAsync()
+        {
+            var oneFilmFranchiseModel = new FilmFranchiseModel()
+            {
+                Id = 1,
+                Franchise = "Marvel",
+                FilmProductor = "Disney",
+                FilmProducer = "Kevin Feige",
+                FirstMovieYear = 2010,
+                LastMovieYear = 2022,
+                Description = "SuperHeros Movies",
+                MovieCount = 22,
+            };
+            var twoFilmFranchiseModel = new FilmFranchiseModel()
+            {
+                Id = 2,
+                Franchise = "DC",
+                FilmProductor = "Warner Bros.",
+                FilmProducer = "Dan Lin",
+                FirstMovieYear = 2013,
+                LastMovieYear = 2021,
+                Description = "SuperHeros Movies",
+                MovieCount = 8,
+            };
+            var FilmFranchisesEnumerable = new List<FilmFranchiseModel>() { oneFilmFranchiseModel, twoFilmFranchiseModel } as IEnumerable<FilmFranchiseModel>;
+
+
+            var franchiseServiceMock = new Mock<IFilmFranchiseService>();
+            franchiseServiceMock.Setup(f => f.GetFranchisesAsync("asc","id")).ReturnsAsync(FilmFranchisesEnumerable);
+            var fileService = new FileService();
+
+            var franchiseController = new FilmFranchisesController(franchiseServiceMock.Object, fileService);
+
+            var result = await franchiseController.GetFranchisesAsync("asc", "id");
+            var okResult = result.Result as OkObjectResult;
+            var franchisesList = okResult.Value as List<FilmFranchiseModel>;
+            
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.NotEmpty(franchisesList);
+            Assert.Contains(franchisesList, franchise => franchise.Id == oneFilmFranchiseModel.Id);
+        }
+    }
+}
